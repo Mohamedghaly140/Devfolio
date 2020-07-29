a9a9fb5dc0e93025d784a08f346de5e61b8c2e9a
mohamedghaly140 <mohamedghaly140@gmail.com>
2020-07-29 21:16:05 +0200

    change navbar on scrolling

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 let typed = new Typed('#typed', {
-  strings: ["Morgan Freeman." ,"A Graphic Designer.", "A JavaScript Developer."],
+  strings: [
+    'Morgan Freeman.',
+    'A Graphic Designer.',
+    'A JavaScript Developer.',
+  ],
   typeSpeed: 60,
   backSpeed: 40,
   loop: true,
+});
+
+window.addEventListener('scroll', function () {
+  let header = document.querySelector('#nav');
+  let toggleBtn = document.querySelector('.navbar-toggler');
+  let windowPosition = window.scrollY > 0;
+  header.classList.toggle('scrolling-active', windowPosition);
+  toggleBtn.classList.toggle('dark', windowPosition);
 });
